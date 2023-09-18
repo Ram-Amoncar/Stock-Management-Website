772972b12d0c56487f759e27c9f68db2861a89dd
Ram-Amoncar <ramamonkar444@gmail.com>
2023-09-18 21:10:48 +0530

    Outline

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$req = $_SERVER['REQUEST_URI'];
+
+$req = substr($req,21); // remove it in hosting
+
+switch ($req) {
+
+    case '':
+    case '/':
+    case '/login':
+        require './views/login.php';
+        break;
+
+    case '/register':
+        require './views/register.php';
+        break;
+
+    // case '/views/authors':
+    //     require __DIR__ . '/views/authors.php';
+    //     break;
+
+    // case '/about':
+    //     require __DIR__ . '/views/aboutus.php';
+    //     break;
+
+    default:
+        require './views/error.php';
+        break;
+
+}
