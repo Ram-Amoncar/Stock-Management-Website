f1019220817d164a49bd5382c7e38f2dfd002144
StevenR24 <120272435+StevenR24@users.noreply.github.com>
2023-09-20 00:45:48 +0530

    php for login (Storing values)

--- views/login.php
+++ views/login.php
@@ -6,12 +6,16 @@
     <title>Login</title>
 </head>
 <body>
-    <form>
-        <label for="Username">Username:</label>
-        <input type="text" id="Username"><br>
-        <label for="Pass">Password:</label>
-        <input type="text" id="Pass"><br>
+    <form action="#" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username"><br>
+        <label for="pass">Password:</label>
+        <input type="text" id="pass"><br>
         <button type="button"><b>Login</b></button>
     </form>
 </body>
 </html>
+<?php
+    $userN=$_POST["username"]
+    $pass=$_POST["pass"]
+?>
