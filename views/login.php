82e84beff0a8118a2f6076b06a4674f2b143668c
StevenR24 <120272435+StevenR24@users.noreply.github.com>
2023-09-20 00:40:11 +0530

    register page

--- views/login.php
+++ views/login.php
@@ -6,12 +6,12 @@
     <title>Login</title>
 </head>
 <body>
-<center><form>
+    <form>
         <label for="Username">Username:</label>
         <input type="text" id="Username"><br>
         <label for="Pass">Password:</label>
         <input type="text" id="Pass"><br>
         <button type="button"><b>Login</b></button>
-    </form></center>
+    </form>
 </body>
 </html>
--- views/register.php
+++ views/register.php
@@ -6,6 +6,30 @@
     <title>Registeration</title>
 </head>
 <body>
-    
+    <form action="#" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username"><br>
+        <label for="email">Email:</label>
+        <input type="Email" name="email"><br>
+        <label for="pass1">Password:</label>
+        <input type="password" name="pass1" id="pass1"><br>
+        <label for="pass2">Conform Password:</label>
+        <input type="password" name="pass2" id="pass2"><br>
+        <button type="submit" onclick="checkPass()">Register</button>
+    </form>
+    <script>
+        function checkPass(){
+            let pass1=document.getElementById("pass1").value;
+            let pass2=document.getElementById("pass2").value;
+            if(pass1!=pass2){
+                alert("conform password and password fields do not match");
+            }
+        }
+    </script>
 </body>
 </html>
+<?php
+    $user=$_POST["username"];
+    $email=$_POST["email"];
+    $pass=$_POST["pass1"];
+?>
