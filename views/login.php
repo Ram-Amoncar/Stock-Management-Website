21f60e70f0bb9a282f8cb77e9a7cccb18891c69d
Ram-Amoncar <ramamonkar444@gmail.com>
2023-09-20 16:35:31 +0530

    Optimization for register and login page

--- views/login.php
+++ views/login.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
     <form action="#" method="post">
         <label for="username">Username:</label>
@@ -14,8 +16,9 @@
         <button type="button"><b>Login</b></button>
     </form>
 </body>
+
 </html>
 <?php
-    $userN=$_POST["username"]
-    $pass=$_POST["pass"]
+$userN = $_POST["username"];
+$pass = $_POST["pass"];
 ?>
--- views/register.php
+++ views/register.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registeration</title>
 </head>
+
 <body>
     <form action="#" method="post">
         <label for="username">Username:</label>
         <input type="text" name="username"><br>
         <label for="email">Email:</label>
         <input type="Email" name="email"><br>
-        <label for="pass1">Password:</label>
-        <input type="password" name="pass1" id="pass1"><br>
-        <label for="pass2">Conform Password:</label>
-        <input type="password" name="pass2" id="pass2"><br>
+        <label for="pass">Password:</label>
+        <input type="password" name="pass" id="pass"><br>
+        <label for="cPass">Conform Password:</label>
+        <input type="password" name="cPass" id="cPass"><br>
         <button type="submit" onclick="checkPass()">Register</button>
     </form>
     <script>
-        function checkPass(){
-            let pass1=document.getElementById("pass1").value;
-            let pass2=document.getElementById("pass2").value;
-            if(pass1!=pass2){
+        function checkPass() {
+            let pass1 = document.getElementById("pass").value;
+            let pass2 = document.getElementById("cPass").value;
+            if (pass1 != pass2) {
                 alert("conform password and password fields do not match");
             }
         }
     </script>
 </body>
+
 </html>
 <?php
-    $user=$_POST["username"];
-    $email=$_POST["email"];
-    $pass=$_POST["pass1"];
+$user = $_POST["username"];
+$email = $_POST["email"];
+$pass = $_POST["pass"];
 ?>
