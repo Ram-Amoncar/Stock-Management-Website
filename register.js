627c6b08ef56a7bf4f0f00fd0560a03a87632481
Ram-Amoncar <ramamonkar444@gmail.com>
2023-09-22 16:17:46 +0530

    validation for registration page

---
+++ register.js
@@ -0,0 +1,28 @@
+function validate() {
+    let username = document.forms["RegForm"]["username"].value;
+    let email = document.forms["RegForm"]["email"].value;
+    let pass1 = document.forms["RegForm"]["pass"].value;
+    let pass2 = document.forms["RegForm"]["cPass"].value;
+    console.log(pass1,pass2)
+    if(username.length==0){
+        alert("Enter username");
+        return false;
+    } 
+    else if(email.length==0){
+        alert("Enter email");
+        return false;
+    } 
+    else if(pass1.length==0){
+        alert("Enter password");
+        return false;
+    }else if(pass2.length==0){
+        alert("Enter confirm password");
+        return false;
+    } 
+    else if (pass1 != pass2) {
+        alert("Password fields do not match");            
+        return false;
+    }else{
+        return true;
+    }
+}
--- register.php
+++ register.php
@@ -5,40 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registeration</title>
+    <script src="register.js" defer></script>
 </head>
 
 <body>
-    <form action="#" method="post">
-        <label for="username">Username:</label>
+    <form name="RegForm" action="register.php" method="post" onsubmit="return validate()">
+        <label for="userN">Username:</label>
         <input type="text" name="username"><br>
-        <label for="email">Email:</label>
-        <input type="Email" name="email"><br>
-        <label for="pass">Password:</label>
-        <input type="password" name="pass" id="pass"><br>
-        <label for="cPass">Conform Password:</label>
-        <input type="password" name="cPass" id="cPass"><br>
-        <button type="submit" onclick="checkPass()" name="btnRegister">Register</button>
+        <label for="emailID">Email:</label>
+        <input type="email" name="email"><br> 
+        <label for="password">Password:</label>
+        <input type="password" name="pass"><br>
+        <label for="conPass">Conform Password:</label>
+        <input type="password" name="cPass"><br>
+        <input type="submit" name="btnRegister" value="Register" >
     </form>
-    <script>
-        function checkPass() {
-            checkIsEmpty();
-            let pass1 = document.getElementById("pass").value;
-            let pass2 = document.getElementById("cPass").value;
-            if (pass1 != pass2) {
-                alert("conform password and password fields do not match");
-            }
-        }
-        function checkIsEmpty(){
-            let username=document.getElementsByName("username").vlaue;
-            let email =document.getElementsByName("email").vlaue;
-            if(username.length==0){
-                alert("Enter username");
-            }
-            if(email.length==0){
-                alert("Enter email");
-            }
-        }
-    </script>
 </body>
 
 </html>
@@ -50,7 +31,7 @@
     $user = $_POST["username"];
     $email = $_POST["email"];
     $pass = $_POST["pass"];
-    $ut->add($user,$pass,$email);
-    echo"<script>alert('Account Created')</script>";
+    if ($ut->add($user,$pass,$email)) echo"<script>alert('Account Created')</script>";
+    unset($_POST["btnRegister"]);
  }
 ?>
