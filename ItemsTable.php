09f035a452dcfe8cb0a2ec946f180a8c6b700519
StevenR24 <120272435+StevenR24@users.noreply.github.com>
2023-09-27 20:08:59 +0530

    bug fixing and testing

--- ItemsTable.php
+++ ItemsTable.php
@@ -6,7 +6,7 @@ final class ItemsTable
     function __construct(mysqli $conn)
     {
         $this->conn = $conn;
-        $q = "CREATE TABLE IF NOT EXISTS '$this->tableName'
+        $q = "CREATE TABLE IF NOT EXISTS $this->tableName
             (
             id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
             name VARCHAR(50),
@@ -21,30 +21,30 @@ final class ItemsTable
 
     function add(string $name, int $quantity, int $cpu, int $total_cost, int $user_id)
     {
-        $q = "INSERT INTO '$this->tableName' (`name`,`quantity`,`cpu`,`total_cost`,`user_id`) VALUES
+        $q = "INSERT INTO $this->tableName (`name`,`quantity`,`cpu`,`total_cost`,`user_id`) VALUES
             ( '$name','$quantity','$cpu','$total_cost','$user_id')";
         return mysqli_query($this->conn, $q);
     }
 
     function checkIfIdExists(int $id) : bool
     {
-        $q = "SELECT * FROM '$this->tableName' WHERE id='$id'";
+        $q = "SELECT * FROM $this->tableName WHERE id='$id'";
         $res = mysqli_query($this->conn, $q);
         if($res->fetch_assoc() === null) return false;
         return true;
     }
     function getAll()
     {
-        $q = "SELECT * FROM '$this->tableName'";
+        $q = "SELECT * FROM $this->tableName";
         return mysqli_query($this->conn, $q);
     }
     function update(string $name, int $quantity,int $cpu,int $total_cost, int $id,int $user_id) : bool {
-        $q = "UPDATE '$this->tableName' SET name = '$name' , quantity = '$quantity' , cpu = '$cpu' , total_cost = '$total_cost' WHERE id = '$id' AND user_id = '$user_id'";
+        $q = "UPDATE $this->tableName SET name = '$name' , quantity = '$quantity' , cpu = '$cpu' , total_cost = '$total_cost' WHERE id = '$id' AND user_id = '$user_id'";
         mysqli_query($this->conn, $q);
         return mysqli_affected_rows($this->conn)==1;
     }
     function delete(int $id, int $user_id) : bool {
-        $q = "DELETE FROM '$this->tableName' WHERE id = '$id' AND user_id = '$user_id'";
+        $q = "DELETE FROM $this->tableName WHERE id = '$id' AND user_id = '$user_id'";
         mysqli_query($this->conn, $q);
         return mysqli_affected_rows($this->conn)==1;
     }
--- stock.php
+++ stock.php
@@ -9,7 +9,9 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage your stock here</title>
     <script src="js/alert.js"></script>
+    <script src="js/stock.js"></script>
     <link rel="stylesheet" href="css/stock.css">
+    <link rel="stylesheet" href="css/alert.css">
     <link rel="icon" type="image/x-icon" href="./assets/icon.ico">
 </head>
 
@@ -21,18 +23,18 @@ session_start();
         </a>
     </div>
     <div id="con">
-        <form action="stockFunctions.php" method="post" name="StockForm">
+        <form action="stock.php" method="post" name="StockForm">
             <div class="int-group">
                 <input type="text" name="name" placeholder="Name" >
-                <input type="number" name="quantity" placeholder="Quantity" onchange="findTotal_cost()" >
-                <input type="number" name="cpu" placeholder="Cost per unit" onchange="findTotal_cost()" >
+                <input type="number" name="quantity" placeholder="Quantity" oninput="findTotal_cost()" >
+                <input type="number" name="cpu" placeholder="Cost per unit" oninput="findTotal_cost()" >
                 <input type="number" name="total_cost" placeholder="Total cost" readonly>
             </div>
             <div class="int-group">
                 <input type="submit" value="Add" name="add">
-                <input type="submit" value="Edit" name="edit">
+                <input type="submit" value="Update" name="edit">
                 <input type="submit" value="Delete" name="delete">
-                <input type="submit" value="Clear" name="clear">
+                <input type="reset" value="Clear" name="clear" onclick="alert_message('text fields cleared',3)">
             </div>
         </form>
         <form action="stock.php" method="post">
@@ -41,7 +43,40 @@ session_start();
             </div>
         </form>
     </div>
+    <div id="alert_div" class="alert_div">
+    </div>
 </body>
 
 </html>
+<?php
+include_once("connect_db.php");
+include_once("ItemsTable.php");
+$it = new ItemsTable($conn);
+if (isset($_POST["add"])) {
+    $name = $_POST["name"];
+    $quantity = $_POST["quantity"];
+    $cpu = $_POST["cpu"];
+    $total_cost = $_POST["total_cost"];
+    $res=$it->add($name, $quantity, $cpu, $total_cost, $_SESSION["user_id"]);
+    if($res){
+        echo "<script type='text/javascript'>
+        alert_message('Added Successful',2);
+        </script>";
+    }else{
+        echo "<script type='text/javascript'>
+        alert_message('Failed to Add',1);
+        </script>";
+    }
+    unset($_POST["add"]);
+}
+if(isset($_POST["edit"])){
+    $name = $_POST["name"];
+    $quantity = $_POST["quantity"];
+    $cpu = $_POST["cpu"];
+    $total_cost = $_POST["total_cost"];
+}
+if(isset($_POST["delete"])){
+    
+}
+?>
 
